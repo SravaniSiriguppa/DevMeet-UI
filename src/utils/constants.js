be921f67e94a31fca285a8e069752cf2a81b6ee9
Sravani <59867313+SravaniSiriguppa@users.noreply.github.com>
2026-08-18 21:57:51 +0530

    -fix URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL = "https://devmeet1.onrender.com"
 // "http://localhost:3000";
-export const FRONTEND_URL="https://portfolio0711.web.app"
+export const FRONTEND_URL="https://dev-meet-7.web.app"
 // "http://localhost:5173"
