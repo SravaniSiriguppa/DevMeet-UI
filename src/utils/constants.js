9b5afec53640926cdb81bc4b29466a5b21904b2e
Sravani <59867313+SravaniSiriguppa@users.noreply.github.com>
2026-08-18 21:53:08 +0530

    - Adding the correct URLs to deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
-export const FRONTEND_URL="http://localhost:5173"
+export const BASE_URL = "https://devmeet1.onrender.com"
+// "http://localhost:3000";
+export const FRONTEND_URL="https://portfolio0711.web.app"
+// "http://localhost:5173"
